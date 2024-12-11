ff67127eb58495607c6ac3a37dc2ad811fb92b62
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-12-10 22:49:25 -0400

    feat: add PostgresCreateTaskRepository

---
+++ src/repositories/tasks/create-tasks.ts
@@ -0,0 +1,20 @@
+import { Tasks } from "@prisma/client";
+import { IPostgresCreateTaskRepository } from "../../helpers/repositories/protocols";
+import { CreateTaskParams } from "../../types/tasks";
+import { prisma } from "../../../prisma/prisma";
+
+export class PostgresCreateTaskRepository
+  implements IPostgresCreateTaskRepository
+{
+  async execute(listId: string, params: CreateTaskParams): Promise<Tasks> {
+    const task = await prisma.tasks.create({
+      data: {
+        description: params.description,
+        is_completed: params.is_completed,
+        list_id: listId,
+      },
+    });
+
+    return task;
+  }
+}
---
+++ src/types/tasks.ts
@@ -0,0 +1,5 @@
+export interface CreateTaskParams {
+  description: string;
+  is_completed: boolean;
+  list_id: string;
+}
